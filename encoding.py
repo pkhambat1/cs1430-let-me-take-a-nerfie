64f0130c240de04a7dffd6cd552aa2faaf76d615
Pezanne Khambatta <pkhambat@risd.edu>
2022-05-08 23:52:41 -0400

    small import delete

--- encoding.py
+++ encoding.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from main_nerf import opt
+
 
 class FreqEncoder(nn.Module):
     def __init__(self, input_dim, max_freq_log2, N_freqs,
